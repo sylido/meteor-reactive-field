14751260086ee1185e248cf3b3b100252ab3605c
Danail Gabenski <dgabenski@fortressinfosec.com>
2025-06-25 18:32:03 -0600

    Updating for Meteor 3.x
    Updated to sylido and all the peerlibrary dependencies to sylido
    Updated version to 0.7.0
    Updated all dependencies to versions compatible with Meteor 3.2.2

--- package.js
+++ package.js
@@ -1,56 +1,56 @@
 Package.describe({
-  name: 'peerlibrary:reactive-field',
-  summary: "Reactive field for Meteor",
-  version: '0.6.0',
-  git: 'https://github.com/peerlibrary/meteor-reactive-field.git'
+  name    : "sylido:reactive-field",
+  summary : "Reactive field for Meteor",
+  version : "0.7.0",
+  git     : "https://github.com/sylido/meteor-reactive-field.git"
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('METEOR@1.8.1');
+  api.versionsFrom("METEOR@3.2.2");
 
   // Core dependencies.
   api.use([
-    'ecmascript',
-    'tracker',
-    'reactive-var',
-    'underscore'
+    "ecmascript@0.16.10",
+    "tracker@1.3.4",
+    "reactive-var@1.0.13",
+    "underscore@1.6.4"
   ]);
 
-  api.export('ReactiveField');
+  api.export("ReactiveField");
 
-  api.mainModule('lib.js');
+  api.mainModule("lib.js");
 });
 
 Package.onTest(function (api) {
-  api.versionsFrom('METEOR@1.8.1');
+  api.versionsFrom("METEOR@3.2.2");
 
   // Core dependencies.
   api.use([
-    'coffeescript@2.4.1',
-    'ecmascript',
-    'tracker',
-    'templating@1.3.2',
-    'blaze@2.3.3',
-    'spacebars@1.0.15',
-    'underscore'
+    "coffeescript@2.4.1",
+    "ecmascript",
+    "tracker@1.3.4",
+    "templating@1.3.2",
+    "blaze@3.0.2",
+    "spacebars@1.0.15",
+    "underscore@1.6.4"
   ]);
 
   // Internal dependencies.
   api.use([
-    'peerlibrary:reactive-field'
+    "sylido:reactive-field"
   ]);
 
   // 3rd party dependencies.
   api.use([
-    'peerlibrary:classy-test@0.4.0'
+    "sylido:classy-test@0.4.0"
   ]);
 
   api.addFiles([
-    'tests.coffee'
+    "tests.coffee"
   ]);
 
   api.addFiles([
-    'tests_client.html',
-    'tests_client.coffee'
-  ], 'client');
+    "tests_client.html",
+    "tests_client.coffee"
+  ], "client");
 });
